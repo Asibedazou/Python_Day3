0dcfac2985739336419deb6255dba32f6e454209
Shoko Komatsuzaki <shoko.komatsuzaki@med.uni-jena.de>
2022-02-14 10:27:51 +0100

    test

---
+++ Day3-2.py
@@ -0,0 +1,9 @@
+# weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+# print(weekdays)
+
+Numbers = list(range(16))
+Numbers2 = list(range(5, 26))
+Numbers3 = list(range(0, 21, 2))
+print(Numbers)
+print(Numbers2)
+print(Numbers3)
