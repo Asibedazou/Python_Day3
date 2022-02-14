866b45b9eca8de996b41cd8105b4dc990a284ab6
Shoko Komatsuzaki <shoko.komatsuzaki@med.uni-jena.de>
2022-02-14 10:03:31 +0100

    i added file day3

---
+++ Day3.py
@@ -0,0 +1,17 @@
+# weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+# print(weekdays)
+
+Birthday_Feb = ["Shoko", "Etsuko", "Katrin"]
+Brithday_date_Feb = [27, 2, 10]
+Height = [162, 160, 165]
+Birthday_combined = ["Shoko", 27, "Etsuko", 2, "Katrin", 11]
+Height_combined = ["Shoko", 162, "Etsuko", 160, "Katrin", 165]
+
+birthday = input("What is your name?\n")
+Pos = int(input("which position"))
+print(Birthday_Feb[Pos])
+print(Birthday_Feb)
+print(Brithday_date_Feb)
+print(Height)
+print(Birthday_combined)
+print(Height_combined)
